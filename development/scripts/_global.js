884fafbd8bbf19586faa231fbd77074bd546a934
Anton Shchukin <a.a.shchukin@gmail.com>
2022-05-29 19:11:40 +0700

    test pause

--- development/scripts/_global.js
+++ development/scripts/_global.js
@@ -1,1 +1,22 @@
 const mobileMediaQuery = window.matchMedia('(max-width: 767px)')
+
+
+var $html = $('html');
+var scrollWidth = $(window).outerWidth() - $(window).width();
+
+/* Блокирование страницы */
+
+function lockPage() {
+    if ( ! $html.hasClass('scroll-locked') ) {
+        $html.addClass('scroll-locked');
+        $html.css( 'padding-right', scrollWidth + 'px' );
+    }
+}
+
+function unlockPage() {
+    if ( $html.hasClass('scroll-locked') ) {
+        $html.removeClass('scroll-locked');
+        $html.css( 'padding-right', '' );
+    }
+}
+
